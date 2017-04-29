c64b9344403517c53594560e135d26fde0be6f5a
Markus Blaschke <mblaschke82@gmail.com>
2017-04-29 18:13:53 +0200

    Fix script error

--- tests/serverspec/spec/shared/base/services.rb
+++ tests/serverspec/spec/shared/base/services.rb
@@ -6,26 +6,27 @@ shared_examples 'base::services::additional' do
         'ssh',
         'syslog-ng'
     ].each do |service|
-    describe "service #{service} check" do
-        it "should install #{service} daemon" do
-            cmd = command("docker-service install #{service}")
-            expect(cmd.exit_status).to eq 0
-        end
+        describe "service #{service} check" do
+            it "should install #{service} daemon" do
+                cmd = command("docker-service install #{service}")
+                expect(cmd.exit_status).to eq 0
+            end
 
-        it "should enable #{service} daemon" do
-            cmd = command("docker-service enable #{service}")
-            expect(cmd.exit_status).to eq 0
-        end
+            it "should enable #{service} daemon" do
+                cmd = command("docker-service enable #{service}")
+                expect(cmd.exit_status).to eq 0
+            end
 
-        it "should start #{service} daemon" do
-            cmd = command("docker-service start #{service}")
-            expect(cmd.exit_status).to eq 0
-        end
+            it "should start #{service} daemon" do
+                cmd = command("docker-service start #{service}")
+                expect(cmd.exit_status).to eq 0
+            end
 
-        it "should have running #{service} daemon", :retry => 5, :retry_wait => 3 do
-            cmd = command("docker-service check #{service}")
-            expect(cmd.stdout).to match('ok')
-            expect(cmd.exit_status).to eq 0
+            it "should have running #{service} daemon", :retry => 5, :retry_wait => 3 do
+                cmd = command("docker-service check #{service}")
+                expect(cmd.stdout).to match('ok')
+                expect(cmd.exit_status).to eq 0
+            end
         end
     end
 end
